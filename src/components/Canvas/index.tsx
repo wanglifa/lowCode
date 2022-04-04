4a7bae0273b5b04bb4ad19057578aa6612e130c8
wanglifa <wanglifa1995@gmail.com>
2022-04-04 16:53:53 +0800

    feat: 画布排序

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -5,6 +5,7 @@ import React, {
   useRef,
   useContext,
   useEffect,
+  useCallback,
 } from 'react';
 import Context from '@/utils/context';
 import ComponetRender from '@/components/renderer/ComponentRender';
@@ -12,24 +13,51 @@ const Canvas: FC<any> = () => {
   const { state, dispatch } = useContext(Context);
   const dropRef = useRef(null);
   const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    const currentNode = {
-      ...JSON.parse(event.dataTransfer.getData('nodeData')),
-      nodeIndex: state.nodeLists.length,
-    };
-    event.preventDefault();
-    dispatch({
-      type: 'setNodeLists',
-      nodeLists: state.nodeLists.concat(currentNode),
-    });
-    dispatch({
-      type: 'setCurrentNode',
-      currentNode,
-    });
+    if (event.dataTransfer.getData('nodeData')) {
+      const currentNode = {
+        ...JSON.parse(event.dataTransfer.getData('nodeData')),
+        nodeIndex: state.nodeLists.length,
+      };
+      event.preventDefault();
+      dispatch({
+        type: 'setNodeLists',
+        nodeLists: state.nodeLists.concat(currentNode),
+      });
+      dispatch({
+        type: 'setCurrentNode',
+        currentNode,
+      });
+    } else {
+      console.log(event.dataTransfer);
+    }
   };
   const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
+    console.log(event.target, 'ttt');
     event.dataTransfer.dropEffect = 'move';
   };
+
+  const onDragStart = useCallback(
+    (event: React.DragEvent<HTMLDivElement>, nodeList: any) => {
+      console.log('abc');
+      // event.stopPropagation();
+      // event.preventDefault();
+      event.dataTransfer.dropEffect = 'move';
+      event.dataTransfer.setData('dragData', nodeList);
+    },
+    [],
+  );
+
+  const onDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event.target.dataset.index, 'index');
+  };
+
+  const handleClick = useCallback((nodeList, index) => {
+    dispatch({
+      type: 'setCurrentNode',
+      currentNode: nodeList,
+    });
+  }, []);
   return (
     <div
       className="canvas-wrapper"
@@ -38,7 +66,18 @@ const Canvas: FC<any> = () => {
       onDragOver={onDragOver}
     >
       {state.nodeLists.map((nodeList, index) => (
-        <ComponetRender index={index} {...nodeList} key={index} />
+        <div
+          className={
+            state.currentNode.nodeIndex === nodeList.nodeIndex ? 'selected' : ''
+          }
+          onClick={() => handleClick(nodeList, index)}
+          onDragStart={(event) => onDragStart(event, nodeList)}
+          onDragEnd={onDragEnd}
+          draggable
+          data-index={index}
+        >
+          <ComponetRender index={index} {...nodeList} key={index} />
+        </div>
       ))}
     </div>
   );
